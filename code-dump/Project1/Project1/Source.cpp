31090ad80e84e914e0412531bb1375fd4d050c54
Matthew03192 <hllc12@durham.ac.uk>
2024-04-20 01:30:44 +0100

    Added comments

--- code-dump/Project1/Project1/Source.cpp
+++ code-dump/Project1/Project1/Source.cpp
@@ -6,11 +6,14 @@
 
 using namespace std;
 
-
+//defines character class
 class character {
+    //player number stores number of the player controlling the character
     int playerNumber;
+    //stores the x and y position
     int xPos=50;
     int yPos=20;
+    //defines if it should be shown or hidden
     bool hidden = false;
     public:
         character() {
@@ -20,11 +23,13 @@ class character {
             playerNumber = number;
         }
 
+        //randomly moves the object so far used for testing
         void randomMove() {
             xPos += (rand() % 3) - 1;
             yPos += (rand() % 3) - 1;
         }
 
+        //returns a shape for the object
         sf::CircleShape drawing() {
             sf::CircleShape shape(50.f);
             if (playerNumber == 1) {
@@ -56,6 +61,7 @@ class character {
         }
 };
 
+//used for buttons
 class button {
     int xPos=1000;
     int yPos=20;
@@ -63,17 +69,20 @@ class button {
     int ySize=50.f;
     sf::Texture texture;
     public:
+        //checks if the button is clicked
         bool isClicked(int mousex, int mousey,bool mouseClicked) {
             if ((mousex<xPos+xSize) && (mousex>xPos) && (mousey<yPos + ySize) && (mousey>yPos) && mouseClicked) {
                 return true;
             }
         }
 
+        //assigns a texture
         void assignTexture(const sf::Texture textureI) {
             texture = textureI;
 
         }
         
+        //returns the shape so it can be drawn
         sf::RectangleShape drawing() {
             sf::RectangleShape rectangle(sf::Vector2f(xSize, ySize));
 
@@ -92,63 +101,82 @@ class button {
 
 int main()
 {
+    //text display timer will be used to decide how long text should be displayed for
+    //text currently not working
     int textDisplayTimer=0;
+
+    //loads a font
     sf::Font font;
     if (!font.loadFromFile("arial.ttf"))
     {
         cout << "Hello World!";
     }
+    //loads two textures
+    //texture1 for background
     sf::Texture texture;
+    //texture2 for stop button for music
     sf::Texture texture2;
     texture2.loadFromFile("stop.png");
-    if (!texture.loadFromFile("image.png"))
-    {
-        cout << "Error!";
-    }
-    sf::Sprite sprite;
-    sprite.setTexture(texture);
+    texture.loadFromFile("image.png");
+
+    //sets a sprite with the first texture for the background
+    sf::Sprite background;
+    background.setTexture(texture);
+
+    //sets text attributes
     sf::Text text;
     text.setFont(font);
     text.setFillColor(sf::Color::Green);
     text.setCharacterSize(24);
     text.setStyle(sf::Text::Bold | sf::Text::Underlined);
+
+    //creates an array of 30 characters
+    //characters with player number 0 will not appear
+    //characters with player number 1 will be controlled by the user
     character chr[30];
     for (int i = 0; i < 30; i++) {
         chr[i].setNumber(0);
     }
-    
+    //sets some numbers of characters
     chr[1].setNumber(1);
     chr[2].setNumber(1);
     int nextCharacter = 3;
 
+    //creates the music stop button
     button button1;
     button1.assignTexture(texture2);
 
+    //creates a button to add new characters
     button addNewCharacterButton;
     addNewCharacterButton.setPosition(10, 10);
     
+    //holds if the mouse has been clicked
     bool mouseClicked=false;
 
+    //stores when the mouse is released
+    bool mouseReleased = false;
+
+    //creates the window
     sf::RenderWindow window(sf::VideoMode(1200, 700), "SFML");
 
+    //defines a bar at the top of the screen
     sf::RectangleShape bar(sf::Vector2f(1200, 80));
     bar.setPosition(0, 0);
     bar.setFillColor(sf::Color::Blue);
 
-    sf::CircleShape shape(50.f);
-    shape.setFillColor(sf::Color::Green);
+    //loads music (currently not uploaded) (has been tested)
     sf::Music music;
 
     if (!music.openFromFile("music.wav"))
-        return -1; // error
+        return -1;
     sf::Music music2;
     if (!music2.openFromFile("music2.wav"))
-        return -1; // error
+        return -1;
 
-    bool mouseReleased = false;
-
+    //main loop
     while (window.isOpen())
     {   
+        //checks if the mouse has been clicked
         mouseReleased = false;
         if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
             mouseClicked = true;
@@ -160,27 +188,22 @@ int main()
             mouseClicked = false;
         }
 
+        //random move test stuff (will probably be deleted)
         for (int i = 0; i < 30; i++) {
-            if (chr[i].getNumber() == 2) {
+            if (chr[i].getNumber() == 4) {
                 chr[i].randomMove();
             }
         }
+
+        //checks for window closes
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::Right))
-        {
-            shape.move(0.04f, 0.f);
-            text.setString("Hello world");
-            textDisplayTimer = 0;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::Left))
-        {
-            shape.move(-0.04f, 0.f);
-        }
+
+        //plays music based on num key pressed
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::Num1))
         {
             music.play();
@@ -198,14 +221,18 @@ int main()
 
         }
         
+        //sets mouse position
         sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
-        sf::Vector2f shapePosition = shape.getPosition();
+
+        //checks if mouse over character and player owns character
         for (int i = 0; i < 30; i++) {
             if (chr[i].getNumber() == 1) {
                 if (pow(mousePosition.x - 50 - chr[i].x(), 2) + pow(mousePosition.y - 50 - chr[i].y(), 2) < pow(50, 2)) {
+                    //moves character to mouse position if left click
                     if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
                         chr[i].setPosition(mousePosition.x - 50, mousePosition.y - 50);
                     }
+                    //deletes character if right click
                     if (sf::Mouse::isButtonPressed(sf::Mouse::Right)) {
                         chr[i].setNumber(0);
                     }
@@ -214,12 +241,13 @@ int main()
         }
         
         
-        
+        //stops music if stop button is clicked
         if (button1.isClicked(mousePosition.x, mousePosition.y, mouseReleased) == true) {
             music.stop();
             music2.stop();
         }
 
+        //adds a new character when button is clicked and sorts out some possible issues
         if (addNewCharacterButton.isClicked(mousePosition.x, mousePosition.y, mouseReleased) == true) {
             chr[nextCharacter].setNumber(1);
             nextCharacter += 1;
@@ -234,24 +262,28 @@ int main()
         }
       
         
-        
-        
+        //clears window
         window.clear();
-        window.draw(sprite);
+        //draws background
+        window.draw(background);
+        //draws the bar
         window.draw(bar);
+        //tries to draw text
         if (textDisplayTimer < 10000) {
             window.draw(text);
         }
+
+        //draws the characters
         for (int i = 0; i < 30; i++) {
             if (chr[i].getNumber()!=0) {
                 window.draw(chr[i].drawing());
             }
         }
+        //draws the buttons
         window.draw(button1.drawing());
-        window.draw(text);
         window.draw(addNewCharacterButton.drawing());
         
-        text.setPosition(1, 1);
+        //displays the window
         window.display();
         textDisplayTimer += 1;
     }
